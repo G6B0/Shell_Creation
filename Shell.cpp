1d54926ce716e83c0f4c8ae730357300b7a8961a
G6B0 <146021873+G6B0@users.noreply.github.com>
2025-09-05 11:55:45 -0400

    Soporte de comandos mediante pipes
    Se agregó la función dividir_comandos para reconocer los comandos a usar y su numero, así creando pipes y procesos que se comunican por pipes donde cada proceso ejecuta un comando. Cumpliendo así el KPI 1.2

--- Shell.cpp
+++ Shell.cpp
@@ -23,6 +23,22 @@ std::vector<char*> parsear_input(const std::string &prompt){
     args.push_back(nullptr); //execvp necesita NULL como último argumento
     return args;
 }
+//Función que se encarga de dividir todo el prompt por '|'
+
+std::vector<std::string> dividir_comandos(const std::string &prompt){
+    std::vector<std::string> comandos;
+    std::istringstream iss(prompt);
+    std::string cmd;
+
+    while (std::getline(iss, cmd, '|')){
+        //Quitamos espacios al inicio
+        while (!cmd.empty() && cmd.front() == ' ') cmd.erase(cmd.begin());
+        //Quitamos espacios al final
+        while (!cmd.empty() && cmd.back() == ' ') cmd.erase(cmd.begin());
+        comandos.push_back(cmd);
+    }
+    return comandos;
+}
 
 
 int main()
@@ -30,35 +46,60 @@ int main()
     std::string prompt;
 
     while(true){
-        std::cout <<"$ " << std::flush;
+        std::cout <<"mishell:$ " << std::flush;
         if (!std::getline(std::cin, prompt)) break;
-
         if (prompt == "exit") break;
+        if (prompt.empty()) continue;
+        //Se identifica los comandos a usar en el prompt y cuantos son
+        auto comandos = dividir_comandos(prompt);
+        int n = comandos.size();
 
-        auto args = parsear_input(prompt);
+        //Creación de pipes
 
-        pid_t pid = fork();
-        if (pid == 0){
-            //Proceso hijo
-            if (execvp(args[0], args.data()) == -1){
-                perror("execvp");
+        std::vector<int[2]> pipes (n-1);
+        for (int i = 0 ; i < n-1; i++){
+            if (pipe(pipes[i]) == -1){
+                perror("pipe");
+                exit(1);
             }
-            exit(1);
         }
-        else if(pid > 0){
-            //Proceso padre
-            int status;
-            waitpid(pid, &status, 0);
-        }
-        else {
-            perror("fork");
-        }
+        //Creación de procesos conectados por pipes
+        for (int i = 0; i < n; i++){
+            auto args = parsear_input(prompt);
+            pid_t pid = fork();
+            if (pid == 0){
+                //Proceso Hijo
+                //si no es el primero, lee desde pipe anterior
+                if (i > 0){
+                    dup2(pipes[i-1][0], STDIN_FILENO);
+                }
+                //si no es el ultimo, escribe en pipe actual
+                if (i < n-1){
+                    dup2(pipes[i][1], STDOUT_FILENO);
+                }
 
-        //Liberamos la memoria usada al parsear el argumento
-        for (auto arg : args){
-            delete[] arg;
+                //hijos cierran todas las pipes
+                for (int j = 0; j < n-1; j++){
+                    close(pipes[j][0]);
+                    close(pipes[j][1]);
+                }
+                execvp(args[0], args.data());
+                perror("execvp");
+                exit(1);
+            }
+            //Padre libera memoria de tokens
+            for (auto arg: args) delete[] arg;
+        }
+        //Padre cierra todos los pipes
+        for (int i = 0; i < n-1; i++){
+            close(pipes[i][0]);
+            close(pipes[i][1]);
+        }
+        //Esperar a todos los hijos
+        for (int i = 0; i < n; i++){
+            wait(nullptr);
         }
     }
-
+    
     return 0;
 }
